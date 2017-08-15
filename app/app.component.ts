f4fb354fd0da6e740254ae304b8a641aed88b070
Larry <larryjamestaylor@gmail.com>
2017-08-15 10:25:09 -0700

    install bower/bootstrap and test

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ap-root',
+  selector: 'app-root',
   template: `
+  <div class="container">
   <h1>Shake that App</h1>
+  </div>
   `
 })
 
 export class AppComponent {
-  
+
 }
